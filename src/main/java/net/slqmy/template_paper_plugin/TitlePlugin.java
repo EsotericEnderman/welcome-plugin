2452d4157e16960249a92537fb1a408fccaa3137
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-02 21:35:43 +0100

    Correctly send title and send subtitles

--- src/main/java/net/slqmy/template_paper_plugin/TitlePlugin.java
+++ src/main/java/net/slqmy/template_paper_plugin/TitlePlugin.java
@@ -1,17 +1,19 @@
 package net.slqmy.template_paper_plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.TitlePart;
 import net.md_5.bungee.api.ChatColor;
 
 import java.util.List;
@@ -20,18 +22,18 @@ import java.util.stream.Stream;
 @DefaultQualifier(NonNull.class)
 public final class TitlePlugin extends JavaPlugin implements Listener {
 
-  private final MiniMessage miniMessage = MiniMessage.miniMessage();
-
   private Component titleComponent;
 
   private List<TextComponent> changingSubtitleComponents;
 
-  private int secondsBeforeChaning;
+  private long secondsBeforeChaning;
 
   @Override
   public void onEnable() {
     getDataFolder().mkdir();
     saveDefaultConfig();
+
+    Bukkit.getPluginManager().registerEvents(this, this);
 
     FileConfiguration configuration = getConfig();
 
@@ -39,7 +41,7 @@ public final class TitlePlugin extends JavaPlugin implements Listener {
 
     changingSubtitleComponents = Stream.of((List<String>) configuration.getList("subtitle")).map((string) -> Component.text(ChatColor.translateAlternateColorCodes('&', ""))).toList();
 
-    secondsBeforeChaning = configuration.getInt("seconds-before-changing");
+    secondsBeforeChaning = configuration.getLong("seconds-before-changing");
   }
 
   @EventHandler
@@ -49,7 +51,22 @@ public final class TitlePlugin extends JavaPlugin implements Listener {
     if (status.equals(PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED)) {
       Player player = event.getPlayer();
 
-      player.sendMessage(titleComponent);
+      player.sendTitlePart(TitlePart.TITLE, titleComponent);
+
+      new BukkitRunnable() {
+
+        private int subTitleIndex = 0;
+
+        @Override
+        public void run() {
+
+          player.sendTitlePart(TitlePart.SUBTITLE, changingSubtitleComponents.get(subTitleIndex));
+
+          subTitleIndex++;
+          subTitleIndex %= changingSubtitleComponents.size();
+        }
+        
+      }.runTaskTimer(this, secondsBeforeChaning * 20L, secondsBeforeChaning * 20L);
     }
   }
 }
