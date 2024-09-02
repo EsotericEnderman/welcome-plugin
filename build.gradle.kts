4c09ef98c843ad4d48da876923694a5c7bcf0632
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-02 23:16:55 +0100

    Use a working API version

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ val projectVersionString = version.toString()
 val javaVersion = 21
 val javaVersionEnumMember = JavaVersion.valueOf("VERSION_" + javaVersion)
 
-val paperApiMinecraftVersion = "1.20.4"
-val paperApiVersion = "1.20.4" + "-" + "R0.1-SNAPSHOT"
+val paperApiMinecraftVersion = "1.20"
+val paperApiVersion = paperApiMinecraftVersion + "-" + "R0.1-SNAPSHOT"
 
 java {
   sourceCompatibility = javaVersionEnumMember
